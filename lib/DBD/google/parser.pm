946f5e08bd6dc55511ee159e2934783f428e5fc4
dlc <dlc>
2003-02-20 12:50:09 +0000

    Reworking of function handling code within SQL parser, to accomidate arbitrary functions and methods.

--- lib/DBD/google/parser.pm
+++ lib/DBD/google/parser.pm
@@ -1,18 +1,27 @@
 package DBD::google::parser;
 
 # ----------------------------------------------------------------------
-# $Id: parser.pm,v 1.1 2003/02/14 20:30:12 dlc Exp $
+# $Id: parser.pm,v 1.2 2003/02/20 12:50:09 dlc Exp $
 # ----------------------------------------------------------------------
 
 use strict;
-use vars qw($VERSION);
+use vars qw($VERSION $FIELD_RE $FUNC_RE);
 
-use URI::Escape qw(uri_escape);
+use Carp qw(carp);
+use File::Spec::Functions qw(catfile);
 use HTML::Entities qw(encode_entities);
+use URI::Escape qw(uri_escape);
 
-$VERSION = sprintf "%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/;
+$VERSION = sprintf "%d.%02d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/;
 
-my $FIELD_RE = qr/[a-zA-Z][a-zA-Z0-9_]*/;
+# XXX $FUNC_RE needs to catch methods as well as functions.  Currently
+# catches things like Digest::MD5::md5_hex(title) but will miss methods
+# like URI->new(URL).
+
+$FIELD_RE = '[a-zA-Z][a-zA-Z0-9_]';
+$FUNC_RE = qr/$FIELD_RE*(?:::$FIELD_RE*)*/;
+#my $FUNC_RE = qr/$FIELD_RE*(?:::$FIELD_RE*)*(?:[-]>$FIELD_RE*)?/; # methods?
+   $FIELD_RE = qr/$FIELD_RE*/;
 my @default_columns = sort qw( title URL snippet summary
                                cachedSize directoryTitle
                                hostName directoryCategory
@@ -20,11 +29,12 @@ my @default_columns = sort qw( title URL snippet summary
 my %allowed_columns = map { lc($_) => 1 } @default_columns;
 
 my %functions = (
-    'default'     => sub { shift                  },
-    'uri_escape'  => sub { uri_escape(shift)      },
-    'html_escape' => sub { encode_entities(shift) },
-    'html_strip'  => \&striphtml,
+    'default'       => sub { shift                  },
+    'uri_escape'    => sub { uri_escape(shift)      },
+    'html_escape'   => sub { encode_entities(shift) },
+    'html_strip'    => \&striphtml,
 );
+$functions{''} = $functions{'default'};
 
 # ----------------------------------------------------------------------
 # parse($sql)
@@ -68,7 +78,7 @@ sub parse {
 
                         # Field name, including possible function
                         (?:
-                          ($FIELD_RE\s*\([^)]+\))   # $1 => function
+                          ($FUNC_RE\s*\([^)]+\))   # $1 => function
                         |
                           ($FIELD_RE)               # $2 => field name
                         | (\*)                      # $3 => '*' 
@@ -103,20 +113,59 @@ sub parse {
         }
         elsif ($function) {
             my $original = $function;
-            $original =~ /($FIELD_RE)\s*\((.*?)\)/;
+            $original =~ /($FUNC_RE)\s*\((.*?)\)/;
 
             my ($f, $n) = ($1, $2);
             $n =~ s/(^\s*|\s*$)//g;
 
             unless ($allowed_columns{$n}) {
-                warn "Unknown column name '$n'\n";
+                carp "Unknown column name '$n'\n";
                 next;
             }
 
             if (defined $functions{$f}) {
+                # Common case:
+                #
+                #   SELECT html_strip(title) FROM google ...
+                #
+                # A pre-defined function.
                 $f = $functions{$f};
             }
+            elsif ($f =~ /::/) {
+                # If a user specifies a function like:
+                #
+                #   SELECT Digest::MD5::md5_hex(title) FROM google ...
+                #
+                # This will pick that up.  $FUNC_RE only catches
+                # things in the above format; there should be an
+                # associated $METHOD_RE which will catch things
+                # like:
+                #
+                #   SELECT URI->new(URL) FROM google ...
+                #
+                my ($package, $func) = $f =~ /(.*)::(.*)/;
+                $package = catfile(split '::', $package);
+                $package .= ".pm";
+                print STDERR "\$package => '$package', \$func => '$func'";
+
+                eval "use $package;";
+                if ($@) {
+                    carp "Can't require $package: $@";
+                    $f = $functions{'default'}
+                }
+                else {
+                    if (defined &{"$package\::$func"}) {
+                        $f = \&{"$package\::$func"};
+                    }
+                    else {
+                        $f = $functions{'default'};
+                    }
+                }
+            }
             else {
+                # No function:
+                #
+                #   SELECT title FROM google ...
                 $f = $functions{'default'};
             }
 
@@ -136,7 +185,7 @@ sub parse {
                            original => $2
                     );
             } else {
-                warn "Unknown column name '$2'\n";
+                carp "Unknown column name '$2'\n";
                 next;
             }
         }
